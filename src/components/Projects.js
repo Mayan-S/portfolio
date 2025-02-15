dea84ab1da68dda1e08ba91a548aa21fe0bebb0e
Mayan Saravanabavan <mayansaravanabavan@gmail.com>
2025-02-15 14:00:30 -0500

    commit main

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,50 +6,48 @@ const Projects = () => (
     <h2>Projects</h2>
     <div className="projects-grid">
       <div className="project-card">
-        <div className="project-image-placeholder">
+        <div className="project-image-1">
         </div>
         <div className="project-content">
-          <h3>Coming Soon...</h3>
-          <p>Stay tuned for exciting new projects that will be added in the future!</p>
+          <h3>Dog Life Simulator</h3>
+          <p>Game that simulates the experience of adopting and caring for an aged dog.</p>
           <div className="project-links">
-            <a href="https://mayan-s.github.io/portfolio/#/projects" className="button">GitHub</a>
+            <a href="https://github.com/Mayan-S/dog-life-simulator" target="_blank" rel="noopener noreferrer" className="button">GitHub</a>
           </div>
           <div className="project-tags">
-            <span className="tag">Languages</span>
-            <span className="tag">and</span>
-            <span className="tag">Tools</span>
+            <span className="tag">Java</span>
+            <span className="tag">OOP</span>
           </div>
         </div>
       </div>
       <div className="project-card">
-        <div className="project-image-placeholder">
+        <div className="project-image-2">
         </div>
         <div className="project-content">
-          <h3>Coming Soon...</h3>
-          <p>Stay tuned for exciting new projects that will be added in the future!</p>
+          <h3>Resume Builder</h3>
+          <p>Generate an engineering resume in under 10 minutes as a TeX or PDF file.</p>
           <div className="project-links">
-            <a href="https://mayan-s.github.io/portfolio/#/projects" className="button">GitHub</a>
+            <a href="https://github.com/Mayan-S/resume-builder" target="_blank" rel="noopener noreferrer" className="button">GitHub</a>
           </div>
           <div className="project-tags">
-            <span className="tag">Languages</span>
-            <span className="tag">and</span>
-            <span className="tag">Tools</span>
+            <span className="tag">JavaScript</span>
+            <span className="tag">HTML</span>
+            <span className="tag">CSS</span>
           </div>
         </div>
       </div>
       <div className="project-card">
-        <div className="project-image-placeholder">
+        <div className="project-image-3">
         </div>
         <div className="project-content">
-          <h3>Coming Soon...</h3>
-          <p>Stay tuned for exciting new projects that will be added in the future!</p>
+          <h3>Personal Portfolio</h3>
+          <p>Portfolio that showcases and organises my projects and coursework.</p>
           <div className="project-links">
-            <a href="https://mayan-s.github.io/portfolio/#/projects" className="button">GitHub</a>
+            <a href="https://github.com/Mayan-S/portfolio" target="_blank" rel="noopener noreferrer" className="button">GitHub</a>
           </div>
           <div className="project-tags">
-            <span className="tag">Languages</span>
-            <span className="tag">and</span>
-            <span className="tag">Tools</span>
+            <span className="tag">React</span>
+            <span className="tag">Node.js</span>
           </div>
         </div>
       </div>
