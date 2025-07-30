bbc05b8a51c357be79a9ea714cf3e5bfbf1661e8
Mayan Saravanabavan <mayansaravanabavan@gmail.com>
2025-07-30 02:00:06 -0400

    Edit About Section

--- src/components/About.js
+++ src/components/About.js
@@ -8,10 +8,10 @@ const About = () => {
         <p>About Me</p>
       </div>
       <p>
-        I’m a first-year engineering student at the University of Toronto, currently in the Track One program. This allows me to explore various engineering disciplines before specialising. In my second year, I will major in Electrical Engineering with a minor in Sustainable Energy.
+        I’m a second-year engineering student at the University of Toronto.
       </p>
       <p>
-        Beyond academics, I’m actively involved in many extracurriculars. This includes the University of Toronto Amateur Radio Society, University of Toronto Chemical Vehicles, and UTWind.
+        Under Construction!
       </p>
     </div>
   );
