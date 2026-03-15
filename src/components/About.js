670f96a45ee9de14f1c6e5df704a260ec2b42136
Mayan Saravanabavan <mayansaravanabavan@gmail.com>
2026-03-15 19:58:42 -0400

    new about section

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,13 @@ const About = () => {
         <p>About Me</p>
       </div>
       <p>
-        I am an incoming second-year engineering student at the University of Toronto.
+        I am a second-year Electrical Engineering student at the University of Toronto
+
+        I am interested in careers related to power systems, control systems, and software development
+        
+        Next year, I will pursue a minor in Sustainable Energy to strengthen my understanding of energy systems and their role in building cleaner, more efficient communities.
+
+        I am seeking a Summer 2026 co-op opportunity to gain hands-on experience, work on practical projects, and explore potential career paths after graduation
       </p>
       <p>
         Under Construction!
