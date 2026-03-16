286263a5069ff653e307ba72d85865639a8c2137
Mayan Saravanabavan <mayansaravanabavan@gmail.com>
2026-03-15 20:01:39 -0400

    fixed about section

--- src/components/About.js
+++ src/components/About.js
@@ -8,16 +8,16 @@ const About = () => {
         <p>About Me</p>
       </div>
       <p>
-        I am a second-year Electrical Engineering student at the University of Toronto
-
-        I am interested in careers related to power systems, control systems, and software development
-        
-        Next year, I will pursue a minor in Sustainable Energy to strengthen my understanding of energy systems and their role in building cleaner, more efficient communities.
-
-        I am seeking a Summer 2026 co-op opportunity to gain hands-on experience, work on practical projects, and explore potential career paths after graduation
+        I am a second-year Electrical Engineering student at the University of Toronto        
       </p>
       <p>
-        Under Construction!
+        I am interested in careers related to power systems, control systems, and software development
+      </p>
+      <p>
+        Next year, I will pursue a minor in Sustainable Energy to strengthen my understanding of energy systems and their role in building cleaner, more efficient communities.
+      </p>
+      <p>
+        I am seeking a Summer 2026 co-op opportunity to gain hands-on experience, work on practical projects, and explore potential career paths after graduation
       </p>
     </div>
   );
